4e293017ab7fb2822c18e2a5e314d5b5867d9e45
ismaelmaizon <ismael.maizon@gmail.com>
2024-11-04 14:23:55 -0300

    first commit

---
+++ src/controllers/productos.controller.js
@@ -0,0 +1,271 @@
+import { pool } from "../db.js";
+import {DataTime, generarIDAleatorio} from "../utils.js";
+import fs from 'fs-extra';
+
+
+
+//ver todos los productos
+export const getProductos = async (req, res) => {
+  console.log('ingreso a productos');
+  console.log(req.cookies);
+  try {
+    const [productos] = await pool.query("SELECT * FROM productos");
+    
+    res.send( {status: 200, message: 'succes', response: productos} );
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+//ver un producto
+export const getProducto = async (req, res) => {
+  try {
+    const { idg } = req.params;
+    console.log(idg);
+    const [rows] = await pool.query("SELECT * FROM productos WHERE IdGenerate = ?", [
+      idg,
+    ]);
+
+    if (rows.length <= 0) {
+      return res.status(404).json({ message: "producto not found" });
+    }
+    res.json(rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+//crear producto
+export const createProducto = async (req, res) => {
+  // Accede al archivo subido
+  const file = req.file;
+  // Accede a los otros datos del producto enviados en el formulario
+  const {Tipo, Ancho, Alto, Izq, Derc, Precio_U } = JSON.parse(req.body.prod)
+  const stock = 0
+  const idGenerate = generarIDAleatorio(10)
+  console.log(file);
+  console.log(idGenerate,Tipo, Ancho, Alto, Izq, Derc, Precio_U, stock);
+  let valid = false
+  try {
+    const [productos] = await pool.query("SELECT * FROM productos");
+    productos.map((pac) =>{
+      if (pac.Tipo == Tipo && pac.Ancho == Ancho && pac.Alto == Alto && pac.Derc == Derc && pac.Izq == Izq) {
+        console.log('ya existe');
+        valid = true
+      }
+    })
+    if (valid) {
+      res.status(201).json({ message: 'ya existe el producto'  });
+    } else {
+      if (file == undefined) {
+        try{
+          const [rows] = await pool.query(
+            "INSERT INTO productos (IdGenerate, Tipo, Ancho, Alto, Izq, Derc, Precio_U, stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+            [idGenerate, Tipo, Ancho, Alto, Izq, Derc, Precio_U, stock]
+          );
+          res.status(200).json({ message: 'producto creado con exito' });
+        }catch(err){
+          console.log(err);
+          res.status(400).json({ message: 'problemas al crear el producto', error: err });
+        }
+      }else{
+        try{
+          const [rows] = await pool.query(
+            "INSERT INTO productos (IdGenerate, Tipo, Ancho, Alto, Izq, Derc, Precio_U, stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+            [idGenerate, Tipo, Ancho, Alto, Izq, Derc, Precio_U, stock]
+          );
+          try{
+            const [rows2] = await pool.query(
+              "INSERT INTO imagenes (IdProduct, url) VALUES (?, ?);",
+              [idGenerate, file.filename]
+            );
+            res.status(200).json({ message: 'producto creado con exito' });
+          }catch(err){
+            console.log(err);
+            res.status(400).json({ message: 'problemas al agregar imagen del producto', error: err });
+          }
+        }catch(err){
+          console.log(err);
+          res.status(400).json({ message: 'problemas al crear el producto', error: err });
+        }
+        
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+//Update producto
+export const updateProducto = async (req, res) => {
+  const {IdGenerate, Tipo, Ancho, Alto, Izq, Derc, Precio_U } = req.body
+  console.log(IdGenerate, Tipo, Ancho, Alto, Izq, Derc, Precio_U);
+  const query = `
+  UPDATE productos
+  SET Tipo = ?, Ancho = ?, Alto = ?, Izq = ?, Derc = ?, Precio_U = ?
+  WHERE IdGenerate = ?
+  `;  
+  
+  try {
+    const [rows] = await pool.query("SELECT * FROM productos WHERE IdGenerate = ?", [
+      IdGenerate,
+    ]);
+
+    if(rows.length != 0){
+      try{
+        const [update] = await pool.query(query,[
+          Tipo, Ancho, Alto, Izq, Derc, Precio_U, IdGenerate]
+        );
+        if (update) { 
+          return res.status(200).json({ message: "producto actualizado" });
+        }
+      }catch(err){
+        return res.status(400).json({ message: "problemas al actualizar producto" });
+      }
+
+    }else{
+      return res.status(400).json({ message: "producto no existe" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "problemas para encontrar producto" });
+  }
+};
+
+//ver imagenes del producto
+export const getProductoIms = async (req, res) => {
+  try {
+    const { idg } = req.params;
+    console.log(idg);
+    const [rows] = await pool.query("SELECT * FROM imagenes WHERE IdProduct = ?", [
+      idg,
+    ]);
+
+    if (rows.length <= 0) {
+      return res.status(404).json({ message: "producto not found" });
+    }
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+    
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+//agregar imagen a producto
+export const addImgProducto = async (req, res) => {
+  const file = req.file;
+  // Accede a los otros datos del producto enviados en el formulario
+  const id = req.body.id
+  const originId =  id.slice(1, -1);
+  console.log(originId);
+  try {
+    const [rows] = await pool.query(
+      "INSERT INTO imagenes (IdProduct, url) VALUES (?, ?);",
+      [originId, file.filename]
+    );
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+
+export const deleteProducto = async (req, res) => {
+  //delete FROM lugaresProducto where id_producto = 1;
+  let boolLugProd = false
+  let boolImgProd = false
+  
+  try {
+    const { id } = req.params;
+    const [product] = await pool.query("SELECT * FROM productos WHERE id = ?", [
+      id,
+    ]);
+    console.log(product);
+    //verificar existencia
+    if (product.length == 0) {
+      return res.status(404).json({ message: "product not found" });
+    }
+    //verificar existencia en lugares
+    try{
+      const [lugProd] = await pool.query("delete FROM lugaresProducto where id_producto = ?", [
+        id,
+      ]);
+      if (lugProd.affectedRows >= 0) {
+        boolLugProd = true
+      }
+    }catch(err){
+      console.log(err);
+      return res.status(500).json({ message: "problemas al eliminar producto de lugares" });
+    }
+
+    try{
+      //verificar existencia de imagenes
+      const [imgProd] = await pool.query("delete FROM imagenes where IdProduct = ?", [
+        product[0].IdGenerate,
+      ]);
+      if (imgProd.affectedRows >= 0) {
+        boolImgProd = true
+      }
+    }catch(err){
+      console.log(err);
+      return res.status(500).json({ message: "problemas al eliminar imagenes del producto" });
+    }
+    
+    if (boolImgProd && boolLugProd) {
+      try{
+        const [rows] = await pool.query("delete FROM productos where id = ?", [
+          id,
+        ])
+        if (rows){
+          return res.status(200).json({ message: "producto eliminado completamente" });
+        }
+      }catch(err){
+        console.log(err);
+        return res.status(500).json({ message: "problemas al eliminar producto" });
+      }
+    }else{
+      console.log(boolImgProd);
+      console.log(boolLugProd);
+      
+      return res.status(500).json({ message: "problemas al eliminar alguna informacion del producto" });
+    }
+
+  } catch (error) {
+    console.log(error);
+    
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+
+
+export const deleteImgProducto = async (req, res) => {
+  
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT * FROM imagenes WHERE id = ?", [
+      id,
+    ]);
+    console.log(rows);
+    if (rows) {
+      try{
+        const [row] = await pool.query("delete FROM imagenes WHERE id = ?", [
+          id,
+        ]);
+        const response = fs.remove(`../img/${rows[0].url}`, err => {
+          if (err) return console.error(err)
+          console.log('success!')
+        })
+        console.log(response);
+        res.status(200).json({message: 'imagen eliminada'});
+      }catch(err){
+        console.log(err);
+        return res.status(500).json({ message: "Something goes wrong" });
+      }
+    }else{
+      return res.status(404).json({ message: "imagen not found" });
+    }
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
---
+++ src/routes/productos.routes.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import {
+  addImgProducto,
+  createProducto,
+  deleteImgProducto,
+  deleteProducto,
+  getProducto,
+  getProductoIms,
+  getProductos,
+  updateProducto,
+} from "../controllers/productos.controller.js";
+import __dirname  from '../utils.js'
+import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+
+const router = Router();
+
+// carga de imagen
+const storage = multer.diskStorage({
+  destination: __dirname + '/img',
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + file.originalname)
+  }
+})
+const upload = multer({ storage: storage })
+
+// GET todos los /productos
+router.get("/prod", (req, res)=>{ res.send({status:200, message: 'probando probando'}) });
+
+
+router.get("/productos", getProductos);
+// GET un producto
+router.get("/producto/:idg", getProducto);
+router.get("/productoimg/:idg", getProductoIms);
+// INSERT un producto
+router.post("/producto", upload.single('file') , createProducto);
+// INSERT imagen a producto
+router.post("/addimgProduct", upload.single('file') , addImgProducto);
+//update producto
+router.post("/updateProduct", updateProducto);
+// DELETE un producto
+router.delete("/producto/:id", deleteProducto);
+// DELETE imagen de un producto
+router.delete("/deleteproductoImg/:id", deleteImgProducto);
+
+
+// UPDATE un producto
+
+export default router;
