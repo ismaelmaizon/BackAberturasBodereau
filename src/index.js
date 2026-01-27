b7ee7914c3959094345e487ffec18a379ed5e6d2
ismaelmaizon <ismael.maizon@gmail.com>
2026-01-27 08:42:47 -0300

    modificando PORT de index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import app from "./app.js";
 import { PORT } from "./config.js";
 import __dirname  from './utils.js'
 
-app.listen(8080);
-console.log(`Server on port http://localhost:${PORT}`);
-console.log('dir: ' + __dirname );
+// Host: solo localhost por seguridad
+const HOST = process.env.HOST || "127.0.0.1";
 
+app.listen(PORT, HOST, () => {
+  console.log(`Server on http://${HOST}:${PORT}`);
+  console.log("dir: " + __dirname);
+});
