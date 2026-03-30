c5988feef13ea2eadf1dcf6d14f46d0c4f7ff43a
ismaelmaizon <ismael.maizon@gmail.com>
2026-03-30 12:24:13 -0300

    agregando crypto

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
